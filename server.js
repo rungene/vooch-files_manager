5df2827de83d5111f2fa11494d3484b31927cdbe
rungene <rungenesamuel@gmail.com>
2023-10-20 17:51:49 +0000

    Adding express ability to serve static file and frontend

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index');
 
 const app = express();
 
 app.use(express.json({ limit: '10mb' }));
 app.use('/', routes);
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 const port = process.env.PORT || 5000;
 
