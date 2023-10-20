119d3ce5ff12f38f688a48e4930cdfdf5376e4cf
rungene <rungenesamuel@gmail.com>
2023-10-20 17:53:24 +0000

    Changing connect endpoint to method to post from get

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
+router.post('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
