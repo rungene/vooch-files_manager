be663e5f7e5d2fa2877a7c596a91ced0dfd487e6
rungene <rungenesamuel@gmail.com>
2023-10-22 07:39:15 +0000

    Corrections, implement logout

--- frontend/home.js
+++ frontend/home.js
@@ -25,34 +25,45 @@ document.addEventListener('DOMContentLoaded', async () => {
   const logoutBtn = document.getElementById('logout-btn');
   const errorMessage = document.getElementById('error-message');
   const authToken = localStorage.getItem('authToken');
-  if (authToken) {
-    userEmail.textContent = getUserEmailFromToken(authToken);
+
+  if (!authToken) {
+    // Handle the case where authToken is not available (user is not logged in)
+    window.location.href = '/login.html';
+    return;
   }
-  logoutBtn.addEventListener('click', async () => {
-    localStorage.removeItem('authToken');
 
-    try {
-      const response = await fetch('/disconnect', {
-        method: 'POST',
-        headers: {
-          'X-Token': authToken,
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = await response.json();
+  try {
+    const userEmailFromToken = await getUserEmailFromToken(authToken);
+    userEmail.textContent = userEmailFromToken;
 
-      if (response.status === 204) {
-        // Logout success
-        console.log('User logged out sucessfully');
-        window.location.href = '/login.html';
-      } else {
-        // Logout failed
-        errorMessage.textContent = data.error || 'Logout Failed';
-        console.log('Logout failed');
+    logoutBtn.addEventListener('click', async () => {
+      try {
+        const response = await fetch('/disconnect', {
+          method: 'POST',
+          headers: {
+            'X-Token': authToken,
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.status === 204) {
+          // Logout success
+          console.log('User logged out successfully');
+          localStorage.removeItem('authToken');
+          window.location.href = '/login.html';
+        } else {
+          // Logout failed
+          errorMessage.textContent = 'Logout failed';
+          console.log('Logout failed');
+        }
+      } catch (error) {
+        console.error('Error during logout:', error);
+        errorMessage.textContent = 'An error occurred during logout';
       }
-    } catch (error) {
-      console.error('Error during logout:', error);
-      errorMessage.textContent = 'An error occurred during logout';
-    }
-  });
+    });
+  } catch (error) {
+    // Handle error while retrieving user email (e.g., redirect to login page)
+    console.error('Error retrieving user email:', error);
+    errorMessage.textContent = 'An error occurred while retrieving user email';
+  }
 });
